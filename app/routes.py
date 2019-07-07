ab6fe88b070e574929db6b0cf3cace562a659876
Nikolay Pomytkin <nikolayp2800@gmail.com>
2019-07-07 17:47:07 +0000

    route modifications :

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,10 @@ def index():
     user = {'username': 'Nikolay'}
     return render_template('index.html', title="One-Study", user=user)
 
+@app.route('/about')
+def about():
+    return render_template('index.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html', title="Login")
