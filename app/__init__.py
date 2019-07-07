8e853a6562ad349b9be34dca45bd821fa787d2ee
EC2 Default User <ec2-user@ip-172-31-36-127.ec2.internal>
2019-07-07 02:39:39 +0000

    venv updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
-from flask import Flask 
+from flask import Flask, request
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = 
-login = Fl
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import routes, forms, models
+# This import must be after app is initialized as the routes use the app Flask instance
+from app import routes, models, errors
